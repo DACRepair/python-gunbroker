af344a54558729bd78873c5ce7e959a1e781387e
Derek Vance <DACRepair@gmail.com>
2020-05-22 12:37:33 -0400

    Changes tested fine, upped version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="python-gunbroker",
     description="Gunbroker search scraping library",
-    version="1.0",
+    version="1.1",
 
     author="DACRepair",
     author_email="dacrepair@gmail.com",
